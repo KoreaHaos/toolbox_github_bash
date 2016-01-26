a0115b339cb8faebd98bf72921def76655b55f6d
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-01-26 23:42:32 +0000

    Script commit!

--- bash/toolbox/github/add_commit_push_all.bash
+++ bash/toolbox/github/add_commit_push_all.bash
@@ -1,6 +1,28 @@
+# Introduce the script
+
 printf "\nScript add_commit_push_all.bash has been called!\n\n";
+
+# First, turn on git's credential helper cache function.
+# This makes it so constant user/password entry is not needed.
+# By default, Git will cache your password for 15 minutes.
+# More info : https://help.github.com/articles/caching-your-github-password-in-git/
+
 git config --global credential.helper cache
+
+# Add all, 
+
 git add --all;
+
+# commit,
+
 git commit -m 'Script commit!';
+
+# push,
+
 git push --all;
+
+# say goodbye,
+
 printf "\nadd_commit_push_all.bash is done!\n\n";
+
+# and, that's it!
