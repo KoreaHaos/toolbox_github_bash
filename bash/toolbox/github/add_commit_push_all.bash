7fe61770e19e3b79ff15c08dbf33fb83d28eb3d4
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-01-26 23:31:22 +0000

    Script commit!

--- bash/toolbox/github/add_commit_push_all.bash
+++ bash/toolbox/github/add_commit_push_all.bash
@@ -1,4 +1,4 @@
-printf "\nScript add_commit_push_all.bash has been called!\n";
+printf "\nScript add_commit_push_all.bash has been called!\n\n";
 git add --all;
 git commit -m 'Script commit!';
 git push --all;
