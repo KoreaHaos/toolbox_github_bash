b7b3fb7245ab93c3b950c129a50f962a7306afa0
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-01-27 00:01:57 +0000

    Script commit : - NO MESSAGE! Commited by : bash/toolbox/github/add_commit_push_all.bash

--- bash/toolbox/github/add_commit_push_all.bash
+++ bash/toolbox/github/add_commit_push_all.bash
@@ -5,7 +5,8 @@ printf "\nScript add_commit_push_all.bash has been called!\n\n";
 # That done; first, handle any args' passed in.
 
 if [ -z "$1" ]; then
-_commit_message="Script commit by : $0 - NO MESSAGE!";
+#_commit_message="Script commit by : $0 - NO MESSAGE!";
+_commit_message="Script commit : - NO MESSAGE! Commited by : $0";
 else
 _commit_message="$1 = message. Script commit by : $0.";
 fi
